792c496dee2d6ce6736d1184050e6591db9d5c7b
aluno <aluno@ifpe>
2023-09-15 16:11:35 -0300

    Aula C10 e c11

---
+++ src/Rotas.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
+
+import FormCliente from './views/cliente/FormCliente';
+//import FormEntregador from './views/entregador/FormEntregador';
+import Home from './views/home/Home';
+import FormProduto from './views/produto/formProduto';
+
+function Rotas() {
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={ <Home/> } />
+                <Route path="form-cliente" element={ <FormCliente/> } />
+                <Route path="form-produto" element={ <FormProduto/> } />
+            </Routes>
+        </>
+    )
+}
+
+export default Rotas
--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Container, Grid, Image } from 'semantic-ui-react';
-
+import MenuSistema from '../../MenuSistema';
 export default function Home () {
 
    return(
-       <div>
+    <div>
+
+
+    <MenuSistema />
 
            <div style={{marginTop: '5%'}}>
                <Container>
@@ -17,8 +20,8 @@ export default function Home () {
                               
                                Bem vindo ao sistema <strong>OxeFood</strong> ! <br/>
                                Este sistema foi desenvolvido na disciplina de Desenvolvimento para WEB III. <br/> <br/>
-                               Para acessar o código da <strong>API</strong> do sistema, acesse: <a href='https://github.com/robertoalencar/oxefood-api' target='_blank'> https://github.com/robertoalencar/oxefood-api </a> <br/> <br/>
-                               Para acessar o código do <strong>Módulo WEB</strong>, acesse: <a href='https://github.com/robertoalencar/oxefood-web' target='_blank'> https://github.com/robertoalencar/oxefood-web </a>
+                               Para acessar o código da <strong>API</strong> do sistema, acesse: <a href='https://github.com/DarlingAlpha/oxefood-api-gabriel' target='_blank'> https://github.com/DarlingAlpha/oxefood-api-gabriel </a> <br/> <br/>
+                               Para acessar o código do <strong>Módulo WEB</strong>, acesse: <a href='https://github.com/DarlingAlpha/oxefood-web-gabriel' target='_blank'> https://github.com/DarlingAlpha/oxefood-web-gabriel </a>
 
                            </Grid.Column>
                        </Grid.Row>
